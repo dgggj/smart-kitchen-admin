3fee1aa2e042167de2bb1f14245e9c814dd20bef
dgggj <1164695032@qq.com>
2022-04-17 15:05:21 +0800

    add api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,35 @@
 <script setup>
 import { ref } from 'vue'
+import {sayHello} from "../Api/hello";
 
-defineProps({
-  msg: String
-})
 
 const count = ref(0)
+const message=ref("智能厨房安全监控系统后台")
+const hi=()=>{
+  sayHello().then(res=>{
+    message.value=res.data
+  })
+
+}
+
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ message }}</h1>
+  <div class="button-container">
+    <q-btn color="primary" @click="count++">点击次数{{count}}</q-btn>
+    <br>
+    <br>
+    <q-btn color="primary" @click="hi">请求接口</q-btn>
+  </div>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped>
 a {
   color: #42b983;
 }
+
 </style>
+
